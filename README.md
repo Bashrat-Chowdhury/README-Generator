50555bdbd0c74811e8dbcd9c59d5b69aea6252f1
bashratchy <bashrat03@gmail.com>
2023-08-11 22:30:05 -0400

    Added code to create README file

---
+++ README.md
@@ -0,0 +1,9 @@
+{
+	"Title": "ok",
+	"description": "ok",
+	"Table of Contents": "ok",
+	"Installation": "ok",
+	"Usage": "ok",
+	"License": "None",
+	"Questions": "ok"
+}
--- index.js
+++ index.js
@@ -5,60 +5,68 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 // const questions = [];
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "Title",
-    message: "What is the title of your project?",
-  },
-  {
-    type: "input",
-    name: "Description",
-    message:
-      "Provide a short description explaining the what, why, and how of your project.",
-    name: "description",
-  },
-  {
-    type: "input",
-    name: "tableOfContents",
-    message:
-      "If your README is long, add a table of contents to make it easy for users to find what they need.",
-  },
-  {
-    type: "input",
-    name: "Installation",
-    message:
-      "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-  },
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "Title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "Description",
+      message:
+        "Provide a short description explaining the what, why, and how of your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      name: "Table of Contents",
+      message:
+        "If your README is long, add a table of contents to make it easy for users to find what they need.",
+    },
+    {
+      type: "input",
+      name: "Installation",
+      message:
+        "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    },
 
-  {
-    type: "input",
-    name: "Usage",
-    message: "Provide instructions and examples for use.",
-  },
-  {
-    type: "list",
-    message: "Chose a license for your project.",
-    name: "contact",
-    choices: ["MIT", "GNU", "None"],
-  },
-  {
-    type: "input",
-    name: "Usage",
-    message: "Provide instructions and examples for use.",
-  },
-  {
-    type: "input",
-    name: "Questions",
-    message: "Enter your GitHub username to link your Github profile.",
-  },
-  {
-    type: "input",
-    name: "Questions",
-    message:
-      "Enter your email address to users can contact you with additional questions.",
-  },
-]);
+    {
+      type: "input",
+      name: "Usage",
+      message: "Provide instructions and examples for use.",
+    },
+    {
+      type: "list",
+      message: "Chose a license for your project.",
+      name: "License",
+      choices: ["MIT", "GNU", "None"],
+    },
+    {
+      type: "input",
+      name: "Usage",
+      message: "Provide instructions and examples for use.",
+    },
+    {
+      type: "input",
+      name: "Questions",
+      message: "Enter your GitHub username to link your Github profile.",
+    },
+    {
+      type: "input",
+      name: "Questions",
+      message:
+        "Enter your email address to users can contact you with additional questions.",
+    },
+  ])
+  .then((data) => {
+    const filename = `README.md`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("README file created!")
+    );
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
